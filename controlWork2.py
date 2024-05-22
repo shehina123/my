3ec1de4528a5d4a00815a7ec28af54bb754c1f85
shehina123 <156430391+shehina123@users.noreply.github.com>
2024-05-22 12:29:37 +0000

    controlwork2

---
+++ controlWork2.py
@@ -0,0 +1,4 @@
+s='informatics'
+#s1='romantics'
+s1=s[4]+s[3]+s[5:7]+s[1]+s[7:10]+s[10]
+print=(f'{s1}')
--- lesson_12/ex_1.py
+++ lesson_12/ex_1.py
@@ -1,4 +1,4 @@
 s='informatics'
 #s2='romantics'
-s1=[2:7] #'forma'
+s1=s[2:7] #'forma'
 s2=s[4]+s[3]+s[5:7]+s[1]+s[:]
